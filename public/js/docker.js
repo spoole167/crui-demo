f7c8caf9d61ee7a49eaac0d2dc4f8ef1ff783083
Steve Poole <spoole167@googlemail.com>
2019-10-16 21:19:56 +0100

    stuff

--- public/js/docker.js
+++ public/js/docker.js
@@ -19,65 +19,8 @@
 //
 
 
-checkState=function(port) {
-
-  var v=$("#hidden-"+port)
-  if (v.length) {
-      if(v.attr("src")==null) {
-        console.log("assigned for "+port)
-        v.attr("src","http://localhost:"+port+"/resources/images/pets.png")
-        $("#port-"+port).attr("src","http://localhost:"+port)
-      } else {
-
-        $("#port-"+port).attr("src","http://localhost:"+port)
-      }
-  }
-  else {
-    console.log("no element for "+port)
-  }
-
-
-}
-
 var client_state = {
   action: "",
   state:"init",
   ports: []
-}
-
-
-poller=function() {
-
-    var ports=client_state.ports
-    if(ports!=null ) {
-      for(var i=0;i<ports.length;i++) {
-        checkState(ports[i])
-      }
-    }
-}
-
-// setup
-// reset button states
-
-
-
-updateUI2=function() {
-  console.log("update ui")
-  console.log(config)
-  $( "#scores" ).empty()
-  for(var i=0;i<config.length;i++) {
-    var q=config[i]
-    var fid="port-"+q.expose
-    var entry=$("<iframe>", {id: fid, class: "w-100 embed-responsive-item", src:'http://localhost:3000/get_ready.html' });
-    $( "#scores").append(entry)
-    var img=document.createElement('img')
-    img.id="hidden-"+q.expose
-    img.onload = function(){  console.log("loaded "+img.id ); };
-    img.onerror = function(err ){
-        console.log("failed "+img.id)
-        console.log(err)
-    };
-    $( "#hidden").append(img)
-  }
-
 }
